b59cce5f01cfc06fa799103edc64458a31ac8023
ErikaH18 <dcoem@hotmail.com>
2021-12-13 20:49:40 -0600

    functions practice

---
+++ Module2/functions.js
@@ -0,0 +1,10 @@
+//General Fuction Syntax
+// function <nameOfFunction> (<paramaters) {}
+
+function sum(num1, num2){
+    console.log(num1+num2);
+    return num1+num2;
+}
+
+const result = sum(4, 9);
+console.log(result);
